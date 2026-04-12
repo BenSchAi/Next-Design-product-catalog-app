da49fd3bd7ae00d9fd31b94fe7037dc22c211f0d
BenSchAi <ben@pandapro.co.il>
2026-04-12 09:29:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -296,7 +296,7 @@ MONTH_NAMES = {
 }
 
 def _parse_to_uniform_date(text):
-    t = text.strip().replace('.', '')
+    t = text.strip().replace('.', '').replace(',', '')
     m = re.search(
         r'(\d{1,2})(?:st|nd|rd|th)?\s*[,\s]*([A-Za-z]{3,9})\s*[,\s]*(\d{4})',
         t, re.IGNORECASE
